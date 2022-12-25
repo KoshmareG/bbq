fc634948267e454f04429d3548a9772de962790c
KoshmareG <koshmareg@yandex.ru>
2022-12-25 18:18:53 +0700

    Add notice and i18n to controllers

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -21,7 +21,7 @@ class EventsController < ApplicationController
     @event = current_user.events.build(event_params)
 
     if @event.save
-      redirect_to event_path(@event), notice: 'Событие создано'
+      redirect_to event_path(@event), notice: I18n.t('controllers.events.created')
     else
       render :new
     end
@@ -29,7 +29,7 @@ class EventsController < ApplicationController
 
   def update
     if @event.update(event_params)
-      redirect_to event_path(@event), notice: 'Событие изменено'
+      redirect_to event_path(@event), notice: I18n.t('controllers.events.updated')
     else
       render :edit
     end
