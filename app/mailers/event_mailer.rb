74e1fcf4c3d8ead15622c64d2dfca60a3311ba54
KoshmareG <koshmareg@yandex.ru>
2023-01-04 01:07:23 +0700

    New desing for photo mail

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -4,20 +4,20 @@ class EventMailer < ApplicationMailer
     @name = subscription.user_name
     @event = event
 
-    mail to: event.user.email, subject: "#{t('.subscription.new_subscription')} #{event.title}"
+    mail to: event.user.email, subject: "#{t('.subscription.new_subscription')} \"#{event.title}\""
   end
 
   def comment(event, comment, email)
     @comment = comment
     @event = event
 
-    mail to: email, subject: "#{t('.comment.new_comment')} #{event.title}"
+    mail to: email, subject: "#{t('.comment.new_comment')} \"#{event.title}\""
   end
 
   def photo(event, photo, email)
     @event = event
     @photo = photo
 
-    mail to: email, subject: "Новое фото в #{event.title}"
+    mail to: email, subject: "#{t('.photo.new_photo')} \"#{event.title}\""
   end
 end
