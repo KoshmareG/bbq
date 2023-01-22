c34222f37127046c586a3ac02d938db2bc98d6b7
KoshmareG <koshmareg@yandex.ru>
2023-01-22 17:58:00 +0700

    Fix lightbox

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import * as bootstrap from "bootstrap"
 import "@hotwired/turbo-rails"
-import "./controllers/hello_controller.js"
-import "./map.js"
-import "./lightbox.js"
+import "./controllers/"
+import "./map"
+require("bs5-lightbox")
