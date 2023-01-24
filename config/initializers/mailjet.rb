013c277d4b05a58739b9f4105d6a2b7699f64d66
KoshmareG <koshmareg@yandex.ru>
2023-01-25 00:57:18 +0700

    Change delivery method to smtp

--- config/initializers/mailjet.rb
+++ config/initializers/mailjet.rb
@@ -1,6 +1,6 @@
 Mailjet.configure do |config|
   config.api_key = Rails.application.credentials.dig(:mailjet, :api_key)
   config.secret_key = Rails.application.credentials.dig(:mailjet, :secret_key)
-  config.default_from = 'koshmareg@gmail.com'
+  config.default_from = Rails.application.credentials.dig(:mailjet, :email)
   config.api_version = 'v3.1'
 end
