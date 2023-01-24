6f0d94df6017cfd1c5bd314e59a65577601b465a
KoshmareG <koshmareg@yandex.ru>
2023-01-24 19:11:00 +0700

    Change capfile

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,9 +6,10 @@ set :repo_url, 'git@github.com:KoshmareG/bbq.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :branch, ENV.fetch("BRANCH", "staging")
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, '/home/deploy/bbq'
+set :deploy_to, "/home/deploy/#{fetch :application}"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -24,7 +25,7 @@ set :deploy_to, '/home/deploy/bbq'
 append :linked_files, 'config/database.yml', 'config/master.key'
 
 # Default value for linked_dirs is []
-append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor', 'storage'
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -33,7 +34,7 @@ append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor', 's
 # set :local_user, -> { `git config user.name`.chomp }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 5
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
