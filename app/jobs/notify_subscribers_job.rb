16b2387cccf64bf19851cffe09338dd08afb5097
KoshmareG <koshmareg@yandex.ru>
2023-01-28 21:38:32 +0700

    Refactoring notify subscribers job

--- app/jobs/notify_subscribers_job.rb
+++ app/jobs/notify_subscribers_job.rb
@@ -2,18 +2,18 @@ class NotifySubscribersJob < ApplicationJob
   queue_as :default
 
   def perform(record)
-    EventMailer.subscription(record.event, record).deliver_later if record.class == Subscription
-
     if record.class == Photo || record.class == Comment
       all_emails =
         (record.event.subscriptions.map(&:user_email) + [record.event.user.email] - [record.user&.email])
+    end
 
-      case
-      when record.class == Photo
-        all_emails.each { |email| EventMailer.photo(record.event, record, email).deliver_later }
-      when record.class == Comment
-        all_emails.each { |email| EventMailer.comment(record.event, record, email).deliver_later }
-      end
+    case record
+    when Subscription
+      EventMailer.subscription(record.event, record).deliver_later
+    when Photo
+      all_emails.each { |email| EventMailer.photo(record.event, record, email).deliver_later }
+    when Comment
+      all_emails.each { |email| EventMailer.comment(record.event, record, email).deliver_later }
     end
   end
 end
